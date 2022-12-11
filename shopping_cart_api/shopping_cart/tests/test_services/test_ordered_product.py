bd5920a9561f497aaa827b2d31f12fcd3431bbfd
Kirill <pnchkirill@gmail.com>
2022-12-11 16:17:41 +0200

    New tests for shopping cart app

--- shopping_cart_api/shopping_cart/tests/test_services/test_ordered_product.py
+++ shopping_cart_api/shopping_cart/tests/test_services/test_ordered_product.py
@@ -2,6 +2,7 @@ import pytest
 from model_bakery import baker
 from rest_framework.exceptions import NotFound
 
+from exceptions.http_exceptions import StateConflict
 from products.models import Product
 from shopping_cart.models import ShoppingCart, OrderedProduct
 from shopping_cart.services.ordered_product.services import create_ordered_product
@@ -22,3 +23,10 @@ class TestOrderedProductServices:
             baker.make(ShoppingCart), {'slug': product.slug, 'quantity': 12}
         )
         assert OrderedProduct.objects.first() == created_product
+
+    def test_if_product_is_already_in_cart_exception_is_raised(self, create_product):
+        product = create_product()
+        cart = baker.make(ShoppingCart)
+        create_ordered_product(cart, {'slug': product.slug, 'quantity': 12})
+        with pytest.raises(StateConflict):
+            create_ordered_product(cart, {'slug': product.slug, 'quantity': 2})
--- shopping_cart_api/shopping_cart/tests/test_services/test_shopping_cart/test_selectors.py
+++ shopping_cart_api/shopping_cart/tests/test_services/test_shopping_cart/test_selectors.py
@@ -1,8 +1,9 @@
 import pytest
 from model_bakery import baker
+from rest_framework.exceptions import NotFound, ValidationError
 
 from shopping_cart.models import ShoppingCart
-from shopping_cart.services.shopping_cart.selectors import get_shopping_cart_by_id
+from shopping_cart.services.shopping_cart.selectors import get_shopping_cart_by_id, get_shopping_cart_from_cookies
 
 
 @pytest.mark.django_db
@@ -12,3 +13,23 @@ class TestShoppingCartSelectors:
         shopping_cart = baker.make(ShoppingCart)
         baker.make(ShoppingCart)
         assert get_shopping_cart_by_id(shopping_cart.pk) == shopping_cart
+
+    @pytest.mark.parametrize(
+        'wrong_id',
+        ['wrond id', 'a1177b9e-47e8-4091-9150-446ba09b37b7']
+    )
+    def test_if_shopping_cart_id_is_wrong_validation_exception_is_raised(self, wrong_id):
+        with pytest.raises(ValidationError):
+            get_shopping_cart_by_id('wrong id')
+
+    def test_if_shopping_cart_id_cookie_doesnt_exist_not_found_error_raises(self):
+        with pytest.raises(NotFound):
+            get_shopping_cart_by_id('c0cbb41a-4236-4c59-a7cf-87c2e3bacda6')
+
+    def test_if_not_shopping_cart_id_in_cookies_raises_not_found_exception(self):
+        with pytest.raises(NotFound):
+            get_shopping_cart_from_cookies({})
+
+    def test_if_shopping_cart_id_in_cookies_it_will_be_returned(self):
+        cart = baker.make(ShoppingCart)
+        assert get_shopping_cart_from_cookies({'cart_id': cart.pk}) == cart
